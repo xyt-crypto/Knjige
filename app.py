e83257b9067cb2ebf2203c3fc6a3f7ce54d1c5b1
xyt-crypto <tatjana.matkovic@inet.hr>
2026-08-02 21:24:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -95,8 +95,8 @@ else:
     (
                                             options==range(len(knjige)),
                                             index==None,
-                                            placeholder="Odaberite jedno književno djelo",
-                                            format_func=opis_knjige
+                                            placeholder=="Odaberite jedno književno djelo",
+                                            format_func==opis_knjige
     )
 
     if st.button("IZBRIŠI KNJIŽEVNO DJELO"):
